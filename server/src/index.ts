3bd0104384ded985c8205c8c691076e020cecd85
voda0461 <voda@voda.is-a.dev>
2026-08-08 07:37:06 +0530

    feat(server): added redis and discord client.

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,56 @@ import swaggerUi from '@fastify/swagger-ui'
 import { PrismaClient } from './generated/prisma/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
+import { Client, GatewayIntentBits } from 'discord.js';
+import { Redis } from "ioredis";
+
+import serverInfoRoute from "./routes/server-info.route.js"
+
+// discord client setup
+export const discordClient = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildScheduledEvents,
+    ],
+});
 
 const fastify = Fastify({ logger: true })
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
+// connect and destroy discord bot automaticly
+fastify.addHook("onReady", async () => {
+    fastify.log.info("Connecting Discord Bot Client to Gateway...");
+    await discordClient.login(process.env.DISCORD_BOT_TOKEN);
+    fastify.log.info(`Discord Bot logged in as ${discordClient.user?.tag}`);
+});
+
+fastify.addHook("onClose", async () => {
+    fastify.log.info("Disconnecting Discord Bot Client...");
+    discordClient.destroy();
+    fastify.log.info("Discord Bot Client disconnected.");
+});
+
+// redis setup
+export const redis = new Redis(
+    process.env.REDIS_URL ?? "redis://localhost:6379",
+    {
+        maxRetriesPerRequest: 3,
+        enableOfflineQueue: false,
+        enableReadyCheck: true,
+    },
+);
+
+redis.on("connect", () => {
+    fastify.log.info("Connected to Redis successfully");
+});
+
+redis.on("error", err => {
+    fastify.log.error(err, "Redis Connection Error.");
+});
 
 // Register Plugins
 await fastify.register(cors, { origin: '*' })
@@ -20,6 +65,7 @@ await fastify.register(swagger, {
   }
 })
 await fastify.register(swaggerUi, { routePrefix: '/docs' })
+await fastify.register(serverInfoRoute, { prefix: "/api/v1" });
 
 // Basic Health Route
 fastify.get('/api/v1/health', async () => {
